8e3f86b4109b76434403f154c4e281327086f154
Gabriel Wu <gabrielwu84@gmail.com>
2017-08-29 20:54:16 +0800

    widget dev added save params to url feature

--- widget/sandbox-host.js
+++ widget/sandbox-host.js
@@ -5,8 +5,6 @@ define(["modindex"],function(modIdx){
 		var modObj=new modIdx()
 		var modChoices=modObj.availableMods;
 		
-		// get and put params into url here
-		
 		// load mod index and put mods into dropdown menu
 		for (var i in modChoices){
 			var opt=document.createElement("li");
@@ -23,11 +21,39 @@ define(["modindex"],function(modIdx){
 			modsMenu.appendChild(opt);
 		}
 
+		// get and put params into url 
+		var url = new URL(location.href);
+		var m = url.searchParams.get("m");
+		var p = url.searchParams.get("p");
+		// quite a bit of repeated code with multiple sources of truth here.
+		// refactor this in time. (modChoices vs currMod.innerHTML vs m)
+		if(m!=null & modChoices.indexOf(m)>-1){
+			modChoice=m;
+			currMod.innerHTML=m;
+			if(p!=null ){
+				modParams.value=decodeURIComponent(p);
+			} else {
+				require([modBaseAddr+modChoice+".js"],function(mod){
+					var modObj = new mod.author();
+					modParams.value=modObj.coreTemplate;
+				})
+			}
+		}
+
 		runBtn.onclick=function(){
 			jsFile=modChoice;
-			if(jsFile==null){jsFile="null"}
-			params=JSON.parse(modParams.value);
-			youVote.execQn("",jsFile,params,{});
+			paramString=modParams.value;
+			if(jsFile!=null){
+				try{
+					params=JSON.parse(paramString);
+					// need to change index.html here if file name is changed. 
+					// Todo: find a more robust solution.
+					history.pushState({},"", "index.html?m="+jsFile+"&p="+encodeURIComponent(paramString));
+					youVote.execQn("",jsFile,params,{});
+				}catch(e){
+					console.error("params string not json format")
+				}
+			}
 		}
 	}
 })
