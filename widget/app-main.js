3210f6ad15ee5fd05ea7f01888290ee801787889
Gabriel Wu <gabrielwu84@gmail.com>
2018-05-29 14:53:59 +0800

    added d3js to require paths

--- widget/app-main.js
+++ widget/app-main.js
@@ -7,6 +7,7 @@ require.config({
 	],
 	paths:{
 		"jquery":"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min",
+		"d3js":"https://cdnjs.cloudflare.com/ajax/libs/d3/4.2.0/d3.min",
 		"vue":"https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.13/vue",
 		"socket-router":"https://youvote.github.io/socket-router/main",
 	},
